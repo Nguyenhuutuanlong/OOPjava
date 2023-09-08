4b9cbb5b41353924df5083d2967b6e7759cf62c0
Nguyenhuutuanlong <tuanlongqx2.113@gmail.com>
2023-09-08 16:01:51 +0700

    đẩy file

---
+++ Car.java
@@ -0,0 +1,22 @@
+import com.sun.java.swing.plaf.windows.WindowsInternalFrameTitlePane;
+
+public class Car {
+    public int name;
+    public int  width;
+    public int number ;
+    public Car (int name ,int width,int number){
+        this.name= name;
+        this.width = width;
+        this.number=number;
+
+    }
+    public void go(){
+        System.out.println(" đang Chạy");
+
+    }
+    public static void main (String[]args){
+        Car a = new
+    }
+}
+
+
